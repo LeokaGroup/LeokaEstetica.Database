b2092410d692d64f1be711638132c08bbf834e9b
Oyarsa <sierra_93@mail.ru>
2022-09-24 18:10:37 +0300

    Добавление таблиц.

--- dbo/pgsql.leoka_estetica_dev/Tables/[Logs].[LogInfo]/[Logs].[LogInfo]_CreateTable.sql
+++ dbo/pgsql.leoka_estetica_dev/Tables/[Logs].[LogInfo]/[Logs].[LogInfo]_CreateTable.sql
@@ -3,11 +3,14 @@ CREATE TABLE IF NOT EXISTS "Logs"."LogInfo" (
   "ExceptionMessage" TEXT NOT NULL,
   "DateCreated" TIMESTAMP NOT NULL DEFAULT NOW(),
   "Account" VARCHAR(100) NOT NULL,
-  "StackTrace" TEXT NOT NULL
+  "StackTrace" TEXT NULL
 );
 
 ALTER TABLE IF EXISTS "Logs"."LogInfo"
-ADD COLUMN "LogKey" UUID NOT NULL;
+ADD COLUMN IF NOT EXISTS "LogKey" UUID NOT NULL;
 
 CREATE UNIQUE INDEX IF NOT EXISTS "Index_LogInfo_LogKey"
     ON "Logs"."LogInfo" USING btree ("LogKey");
+
+ALTER TABLE IF EXISTS "Logs"."LogInfo"
+ADD COLUMN IF NOT EXISTS "LogLevel" VARCHAR(10) NOT NULL;
