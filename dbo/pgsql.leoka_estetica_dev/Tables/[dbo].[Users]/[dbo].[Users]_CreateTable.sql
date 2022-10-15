3e8899667d3fa036b3ba827d7405a32a52f25d96
Oyarsa <sierra_93@mail.ru>
2022-10-16 01:34:17 +0300

    новых таблиц.

---
+++ dbo/pgsql.leoka_estetica_dev/Tables/[dbo].[Users]/[dbo].[Users]_CreateTable.sql
@@ -0,0 +1,25 @@
+CREATE TABLE IF NOT EXISTS dbo."Users"
+(
+    "UserId" BIGSERIAL,
+    "LastName" VARCHAR(120) NULL DEFAULT '',
+    "FirstName" VARCHAR(120) NULL DEFAULT '',
+    "SecondName" VARCHAR(120) NULL DEFAULT '',
+    "Login" VARCHAR(120) NULL DEFAULT '',
+    "UserIcon" TEXT NULL DEFAULT '',
+    "DateRegister" TIMESTAMP NULL,
+    "Email" VARCHAR(120) NOT NULL,
+    "EmailConfirmed" BOOLEAN NOT NULL DEFAULT FALSE,
+    "PasswordHash" TEXT NOT NULL DEFAULT '',
+    "PhoneNumber" VARCHAR(500) NULL DEFAULT '',
+    "PhoneNumberConfirmed" BOOLEAN NOT NULL DEFAULT FALSE,
+    "TwoFactorEnabled" BOOLEAN NOT NULL DEFAULT FALSE,
+    "LockoutEnd" BOOLEAN NOT NULL DEFAULT FALSE,
+    "LockoutEnabled" BOOLEAN NOT NULL DEFAULT FALSE,
+    "UserCode" uuid,
+    "ConfirmEmailCode" TEXT NULL DEFAULT '',
+    "LockoutEnabledDate" TIMESTAMP NULL,
+    "LockoutEndDate" TIMESTAMP NULL,
+    CONSTRAINT "PK_Users_UserId" PRIMARY KEY ("UserId")
+);
+
+CREATE UNIQUE INDEX "Idx_UserCode" ON dbo."Users" ("UserCode");
