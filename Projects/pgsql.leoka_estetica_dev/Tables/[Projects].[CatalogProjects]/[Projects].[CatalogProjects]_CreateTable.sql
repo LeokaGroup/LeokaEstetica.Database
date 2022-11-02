ac4dccd48f43491dacd12f6833c988f56a740f7c
Oyarsa <sierra_93@mail.ru>
2022-11-02 22:02:32 +0300

    #90 Добавил поле код проекта (гуид).

--- Projects/pgsql.leoka_estetica_dev/Tables/[Projects].[CatalogProjects]/[Projects].[CatalogProjects]_CreateTable.sql
+++ Projects/pgsql.leoka_estetica_dev/Tables/[Projects].[CatalogProjects]/[Projects].[CatalogProjects]_CreateTable.sql
@@ -5,5 +5,6 @@ CREATE TABLE IF NOT EXISTS "Projects"."CatalogProjects"
     "UserId" BIGINT NOT NULL,
     "ProjectDetails" TEXT NOT NULL,
     "ProjectIcon" TEXT NULL,
+    "ProjectCode" UUID NOT NULL DEFAULT '00000000-0000-0000-0000-000000000000',
     CONSTRAINT "PK_CatalogProjects_ProjectId" PRIMARY KEY ("ProjectId")
 );
