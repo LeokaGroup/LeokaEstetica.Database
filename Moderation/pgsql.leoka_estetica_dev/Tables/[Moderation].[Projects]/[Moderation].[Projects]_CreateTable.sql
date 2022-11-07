0f65f1cc0c21558639d6778a49d2ecb1e87bccc4
Oyarsa <sierra_93@mail.ru>
2022-11-08 00:00:22 +0300

    Добавил таблицу модерации проектов.

---
+++ Moderation/pgsql.leoka_estetica_dev/Tables/[Moderation].[Projects]/[Moderation].[Projects]_CreateTable.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS "Moderation"."Projects"
+(
+    "ModerationId" BIGSERIAL,
+    "ProjectId" BIGINT,
+    "DateModeration" TIMESTAMP WITHOUT TIME ZONE not NULL DEFAULT NOW(),
+    CONSTRAINT "PK_Projects_ModerationId" PRIMARY KEY("ModerationId"),
+    CONSTRAINT "FK_Projects_ProjectId" FOREIGN KEY("ProjectId") REFERENCES "Projects"."UserProjects"("ProjectId")
+);
