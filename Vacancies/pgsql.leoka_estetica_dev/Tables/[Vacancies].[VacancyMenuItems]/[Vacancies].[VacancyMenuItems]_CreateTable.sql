5697f4aede488cea200d440c346b4231f71d1336
Oyarsa <sierra_93@mail.ru>
2022-11-13 01:54:23 +0300

    #120 Добавил таблицу для меню вакансий.

--- Vacancies/pgsql.leoka_estetica_dev/Tables/[Vacancies].[VacancyMenuItems]/[Vacancies].[VacancyMenuItems]_CreateTable.sql
+++ Vacancies/pgsql.leoka_estetica_dev/Tables/[Vacancies].[VacancyMenuItems]/[Vacancies].[VacancyMenuItems]_CreateTable.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS "Vacancies"."VacancyMenuItems"
 (
- "ProfileMenuItemId" SERIAL CONSTRAINT "PK_ProfileMenuItems_ProfileMenuItemId" PRIMARY KEY,
+ "ProfileMenuItemId" SERIAL CONSTRAINT "PK_VacancyMenuItems_ProfileMenuItemId" PRIMARY KEY,
     "ProfileMenuItems" JSONB NOT NULL
 );
