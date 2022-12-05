528ce0dca74ae2a47a6ad1a2295c5532f97cf599
Timoshenko A.I <sierra_93@mail.ru>
2022-12-05 23:49:10 +0300

    Добавил таблицы откликов на проекты и статусы откликов.

---
+++ Projects/pgsql.leoka_estetica_dev/Tables/[Projects].[ProjectResponses]/[Projects].[ProjectResponses]_CreateTable.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS "Projects"."ProjectResponses"
+(
+    "ResponseId" BIGSERIAL,
+    "ProjectId" BIGINT NOT NULL,
+    "UserId" BIGINT NOT NULL,
+    "DateResponse" TIMESTAMP NOT NULL DEFAULT NOW(),
+    "ProjectResponseStatuseId" INT NOT NULL,
+    CONSTRAINT "PK_ProjectResponses_ResponseId" PRIMARY KEY ("ResponseId")
+);
