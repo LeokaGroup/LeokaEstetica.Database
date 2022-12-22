ef112deaca1c458ecd295fc88ff2ea67c3b15906
Timoshenko A.I <sierra_93@mail.ru>
2022-12-23 00:04:19 +0300

    Добавил поле для статусов проектов модерации.

--- Moderation/pgsql.leoka_estetica_dev/Tables/[Moderation].[Projects]/[Moderation].[Projects]_CreateTable.sql
+++ Moderation/pgsql.leoka_estetica_dev/Tables/[Moderation].[Projects]/[Moderation].[Projects]_CreateTable.sql
@@ -6,3 +6,7 @@ CREATE TABLE IF NOT EXISTS "Moderation"."Projects"
     CONSTRAINT "PK_Projects_ModerationId" PRIMARY KEY("ModerationId"),
     CONSTRAINT "FK_Projects_ProjectId" FOREIGN KEY("ProjectId") REFERENCES "Projects"."UserProjects"("ProjectId")
 );
+
+ALTER TABLE IF EXISTS "Moderation"."Projects"
+ADD COLUMN IF NOT EXISTS "ModerationStatusId" INT NOT NULL DEFAULT 2,
+    ADD CONSTRAINT "FK_ModerationStatuses_StatusId" FOREIGN KEY ("ModerationStatusId") REFERENCES "Moderation"."ModerationStatuses";
