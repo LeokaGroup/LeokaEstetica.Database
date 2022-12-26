3f00f6d176ba2423769acb9c2c43b922b8d27294
Timoshenko A.I <sierra_93@mail.ru>
2022-12-26 22:03:12 +0300

    Изменения таблиц.

--- Moderation/pgsql.leoka_estetica_dev/Tables/[Moderation].[Vacancies]/[Moderation].[Vacancies]_CreateTable.sql
+++ Moderation/pgsql.leoka_estetica_dev/Tables/[Moderation].[Vacancies]/[Moderation].[Vacancies]_CreateTable.sql
@@ -6,3 +6,7 @@ CREATE TABLE IF NOT EXISTS "Moderation"."Vacancies"
     CONSTRAINT "PK_Vacancies_ModerationId" PRIMARY KEY("ModerationId"),
     CONSTRAINT "FK_Vacancies_VacancyId" FOREIGN KEY("VacancyId") REFERENCES "Vacancies"."UserVacancies"("VacancyId")
 );
+
+ALTER TABLE IF EXISTS "Moderation"."Vacancies"
+ADD COLUMN IF NOT EXISTS "ModerationStatusId" INT NOT NULL DEFAULT 2,
+    ADD CONSTRAINT "FK_ModerationStatuses_StatusId" FOREIGN KEY ("ModerationStatusId") REFERENCES "Moderation"."ModerationStatuses";
