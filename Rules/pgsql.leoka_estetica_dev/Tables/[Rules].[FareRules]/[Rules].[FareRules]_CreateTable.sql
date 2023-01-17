3c07bbe8d0221c42e18b0e501b7a381a19188547
Timoshenko A.I <sierra_93@mail.ru>
2023-01-18 00:32:24 +0300

    Добавил таблицы для правил тарифов.

---
+++ Rules/pgsql.leoka_estetica_dev/Tables/[Rules].[FareRules]/[Rules].[FareRules]_CreateTable.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS "Rules"."FareRules"
+(
+    "RuleId" SERIAL,
+    "Name" VARCHAR(150) NOT NULL,
+    "Label" VARCHAR(200) NOT NULL,
+    "Position" INT NOT NULL DEFAULT 0,
+    "Price" DECIMAL(12,2) NOT NULL,
+    "Currency" VARCHAR(5) NOT NULL DEFAULT 'RUB',
+    "IsPopular" BOOLEAN NOT NULL DEFAULT FALSE,
+    CONSTRAINT "PK_FareRules_RuleId" PRIMARY KEY ("RuleId")
+);
