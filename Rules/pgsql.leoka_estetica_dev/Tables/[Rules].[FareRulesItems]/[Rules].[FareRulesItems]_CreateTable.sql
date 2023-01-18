d5c01c513548ed9bea6e6fcf70eb2a669b5a597b
Timoshenko A.I <sierra_93@mail.ru>
2023-01-19 01:45:39 +0300

    Правки таблиц.

--- Rules/pgsql.leoka_estetica_dev/Tables/[Rules].[FareRulesItems]/[Rules].[FareRulesItems]_CreateTable.sql
+++ Rules/pgsql.leoka_estetica_dev/Tables/[Rules].[FareRulesItems]/[Rules].[FareRulesItems]_CreateTable.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS "Rules"."FareRulesItems"
     "RuleItemId" SERIAL,
     "RuleId" INT,
     "Name" VARCHAR(150) NOT NULL,
-    "Label" VARCHAR(200) NOT NULL,
+    "Label" VARCHAR(200) NULL,
     "Position" INT NOT NULL DEFAULT 0,
     "IsLater" BOOLEAN NOT NULL DEFAULT FALSE,
     CONSTRAINT "PK_FareRulesItems_RuleItemId" PRIMARY KEY ("RuleItemId"),
