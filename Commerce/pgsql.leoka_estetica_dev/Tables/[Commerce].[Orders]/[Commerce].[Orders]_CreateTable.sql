95e782d75f74ddcba0153d1bb9a13821eeca8f18
Timoshenko A.I <sierra_93@mail.ru>
2023-01-26 01:15:42 +0300

    Изменения таблицы заказов.

--- Commerce/pgsql.leoka_estetica_dev/Tables/[Commerce].[Orders]/[Commerce].[Orders]_CreateTable.sql
+++ Commerce/pgsql.leoka_estetica_dev/Tables/[Commerce].[Orders]/[Commerce].[Orders]_CreateTable.sql
@@ -10,5 +10,6 @@ CREATE TABLE IF NOT EXISTS "Commerce"."Orders"
     "Price" DECIMAL(12,2) NOT NULL DEFAULT 0,
     "Currency" VARCHAR(5) NOT NULL DEFAULT 'RUB',
     "PaymentMonth" SMALLINT NOT NULL DEFAULT 1,
+    "PaymentId" VARCHAR(50) NOT NULL,
     CONSTRAINT "PK_Orders_OrderId" PRIMARY KEY ("OrderId")
 );
