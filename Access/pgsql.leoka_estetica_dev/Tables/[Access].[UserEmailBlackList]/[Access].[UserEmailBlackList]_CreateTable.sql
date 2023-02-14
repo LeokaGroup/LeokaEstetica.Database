5c9396c4a2b01ade59b0739d24245b0e8497799c
Timoshenko A.I <sierra_93@mail.ru>
2023-02-14 21:03:57 +0300

    Добавил триггеры для таблиц ЧС.

--- Access/pgsql.leoka_estetica_dev/Tables/[Access].[UserEmailBlackList]/[Access].[UserEmailBlackList]_CreateTable.sql
+++ Access/pgsql.leoka_estetica_dev/Tables/[Access].[UserEmailBlackList]/[Access].[UserEmailBlackList]_CreateTable.sql
@@ -5,3 +5,15 @@ CREATE TABLE IF NOT EXISTS "Access"."UserEmailBlackList"
     "Email" VARCHAR(120) NOT NULL,
     CONSTRAINT "PK_UserEmailBlackList_BlackId" PRIMARY KEY ("BlackId")
 );
+
+CREATE TRIGGER "UserEmailBlackListInsertTrigger"
+AFTER INSERT
+ON "Access"."UserEmailBlackList"
+FOR EACH ROW
+EXECUTE PROCEDURE "Access"."UserEmailBlackListInsert"();
+
+CREATE TRIGGER "UserEmailBlackListDeleteTrigger"
+AFTER DELETE
+ON "Access"."UserEmailBlackList"
+FOR EACH ROW
+EXECUTE PROCEDURE "Access"."UserEmailBlackListDelete"();
--- Access/pgsql.leoka_estetica_dev/Tables/[Access].[UserPhoneBlackList]/[Access].[UserPhoneBlackList]_CreateTable.sql
+++ Access/pgsql.leoka_estetica_dev/Tables/[Access].[UserPhoneBlackList]/[Access].[UserPhoneBlackList]_CreateTable.sql
@@ -5,3 +5,15 @@ CREATE TABLE IF NOT EXISTS "Access"."UserPhoneBlackList"
     "PhoneNumber" VARCHAR(50) NOT NULL,
     CONSTRAINT "PK_UserPhoneBlackList_BlackId" PRIMARY KEY ("BlackId")
 );
+
+CREATE TRIGGER "UserPhoneBlackListInsertTrigger"
+AFTER INSERT
+ON "Access"."UserPhoneBlackList"
+FOR EACH ROW
+EXECUTE PROCEDURE "Access"."UserEmailBlackListInsert"();
+
+CREATE TRIGGER "UserPhoneBlackListDeleteTrigger"
+AFTER DELETE
+ON "Access"."UserPhoneBlackList"
+FOR EACH ROW
+EXECUTE PROCEDURE "Access"."UserPhoneBlackListDelete"();
