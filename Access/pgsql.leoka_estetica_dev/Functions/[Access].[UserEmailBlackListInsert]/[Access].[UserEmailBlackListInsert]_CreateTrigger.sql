8d78688988524ed9a995f2e81def588e791b4990
Timoshenko A.I <sierra_93@mail.ru>
2023-02-15 00:36:39 +0300

    Доработка триггеров.

--- Access/pgsql.leoka_estetica_dev/Functions/[Access].[UserEmailBlackListInsert]/[Access].[UserEmailBlackListInsert]_CreateTrigger.sql
+++ Access/pgsql.leoka_estetica_dev/Functions/[Access].[UserEmailBlackListInsert]/[Access].[UserEmailBlackListInsert]_CreateTrigger.sql
@@ -1,8 +1,8 @@
 CREATE OR REPLACE FUNCTION "Access"."UserEmailBlackListInsert"() RETURNS TRIGGER AS
 $$
 BEGIN
-    INSERT INTO "Access"."UserEmailBlackListShadow" ("DateCreated", "ActionText", "ActionSysName", "UserId")
-    VALUES (NEW."DateCreated", NEW."ActionText", NEW."ActionSysName", NEW."UserId");
+    INSERT INTO "Access"."UserEmailBlackListShadow" ("DateCreated", "ActionText", "ActionSysName", "UserId", "Email")
+    VALUES (NOW(), 'Email пользователя был заблокирован.', 'Add', NEW."UserId", NEW."Email");
 
     RETURN NEW;
 END;
