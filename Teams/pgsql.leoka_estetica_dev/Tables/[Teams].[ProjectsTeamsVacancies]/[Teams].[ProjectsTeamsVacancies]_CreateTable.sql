eecfd013804413d060d126fac4d56e293d497a7f
Timoshenko A.I <sierra_93@mail.ru>
2023-04-24 22:20:53 +0300

    Изменения в таблицах.

--- Teams/pgsql.leoka_estetica_dev/Tables/[Teams].[ProjectsTeamsVacancies]/[Teams].[ProjectsTeamsVacancies]_CreateTable.sql
+++ Teams/pgsql.leoka_estetica_dev/Tables/[Teams].[ProjectsTeamsVacancies]/[Teams].[ProjectsTeamsVacancies]_CreateTable.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS "Teams"."ProjectsTeamsVacancies"
     "VacancyId" BIGINT NOT NULL,
     "IsActive" BOOLEAN NOT NULL,
     "TeamId" BIGINT NOT NULL,
-    CONSTRAINT "PK_ProjectsTeamsVacancies_TeamVacancyId" PRIMARY KEY ("TeamVacancyId"),
-    CONSTRAINT "FK_UserVacancies_VacancyId" FOREIGN KEY ("VacancyId") REFERENCES "Vacancies"."UserVacancies" ("VacancyId"),
-    CONSTRAINT "FK_ProjectsTeams_TeamId" FOREIGN KEY ("TeamId") REFERENCES "Teams"."ProjectsTeams" ("TeamId")
+    CONSTRAINT "PK_ProjectsTeamsVacancies_TeamVacancyId" PRIMARY KEY ("TeamVacancyId")
+    -- CONSTRAINT "FK_UserVacancies_VacancyId" FOREIGN KEY ("VacancyId") REFERENCES "Vacancies"."UserVacancies" ("VacancyId"),
+    -- CONSTRAINT "FK_ProjectsTeams_TeamId" FOREIGN KEY ("TeamId") REFERENCES "Teams"."ProjectsTeams" ("TeamId")
 );
