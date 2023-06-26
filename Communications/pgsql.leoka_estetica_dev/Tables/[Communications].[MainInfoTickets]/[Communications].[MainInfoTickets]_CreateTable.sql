1d53edb3c079be3be611e1690d67dc926327c87a
Timoshenko A.I <sierra_93@mail.ru>
2023-06-26 15:41:32 +0300

    Правки в таблицах тикетов.

--- Communications/pgsql.leoka_estetica_dev/Tables/[Communications].[MainInfoTickets]/[Communications].[MainInfoTickets]_CreateTable.sql
+++ Communications/pgsql.leoka_estetica_dev/Tables/[Communications].[MainInfoTickets]/[Communications].[MainInfoTickets]_CreateTable.sql
@@ -4,7 +4,6 @@ CREATE TABLE IF NOT EXISTS "Communications"."MainInfoTickets"
     "TicketName"     VARCHAR(200) NOT NULL,
     "DateCreated"    TIMESTAMP    NOT NULL DEFAULT NOW(),
     "TicketStatusId" SMALLINT     NOT NULL,
-    "TicketFileId"   BIGINT       NOT NULL,
-    CONSTRAINT "PK_MainInfoTickets_TicketId" PRIMARY KEY ("TicketId"),
-    CONSTRAINT "FK_TicketFiles_FileId" FOREIGN KEY ("TicketFileId") REFERENCES "Communications"."TicketFiles" ("FileId")
+    "TicketFileId"   BIGINT       NULL,
+    CONSTRAINT "PK_MainInfoTickets_TicketId" PRIMARY KEY ("TicketId")
 );
--- Communications/pgsql.leoka_estetica_dev/Tables/[Communications].[TicketStatuses]/[Communications].[TicketStatuses]_CreateTable.sql
+++ Communications/pgsql.leoka_estetica_dev/Tables/[Communications].[TicketStatuses]/[Communications].[TicketStatuses]_CreateTable.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS "Communications"."TicketStatuses"
 (
-    "StatsuId"      SERIAL,
+    "StatusId"      SERIAL,
     "StatusName"    VARCHAR(50) NOT NULL,
     "StatusSysName" VARCHAR(50) NOT NULL,
-    CONSTRAINT "PK_TicketStatuses_StatsuId" PRIMARY KEY ("StatsuId")
+    CONSTRAINT "PK_TicketStatuses_StatusId" PRIMARY KEY ("StatusId")
 );
