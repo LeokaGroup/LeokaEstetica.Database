0c9aea14cca3549405c1e4e92cee030a1de4798d
Timoshenko A.I <sierra_93@mail.ru>
2023-09-16 15:46:35 +0300

    #14821150 Докинул PK.

--- Communications/pgsql.leoka_estetica_dev/Tables/[Communications].[PublicOffer]/[Communications].[PublicOffer]_CreateTable.sql
+++ Communications/pgsql.leoka_estetica_dev/Tables/[Communications].[PublicOffer]/[Communications].[PublicOffer]_CreateTable.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS "Communications"."PublicOffer"
 (
     "OfferId"     SMALLSERIAL,
     "Name"        VARCHAR(200) NOT NULL,
-    "Description" TEXT         NOT NULL
+    "Description" TEXT         NOT NULL,
+    CONSTRAINT "PK_PublicOffer_OfferId" PRIMARY KEY ("OfferId")
 );
